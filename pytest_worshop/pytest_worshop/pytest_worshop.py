7b6d6bc4902bf3d00ab7ec52de1c840d5d60cade
IgnatiusFlo <fireflo@tutanota.com>
2018-09-16 16:02:28 +0100

    part 2 - not finished...

--- pytest_worshop/pytest_worshop/pytest_worshop.py
+++ pytest_worshop/pytest_worshop/pytest_worshop.py
@@ -6,7 +6,18 @@ class Calc:
         return a - b
 
     def mul(self, a, b):
-        return a - b
+        return a * b
 
     def div(self, a, b):
         return a / b
+
+    def zero(self, a, b):
+        if a or b == 0:
+            return "inf"
+
+    def is_empty(self, a):
+        if a == []:
+            return "is empty"
+
+    def average(selfs, a):
+
--- pytest_worshop/tests/test_pytest_worshop.py
+++ pytest_worshop/tests/test_pytest_worshop.py
@@ -1,4 +1,4 @@
-
+import pytest
 import pytest_worshop.pytest_worshop as pt
 
 def test_init():
@@ -34,5 +34,51 @@ def test_sub():
 
     assert result == -1
 
+def test_mul():
+    c = pt.Calc()
 
+    result = c.mul(4, 5)
 
+    assert result == 20
+
+def test_div():
+    c = pt.Calc()
+
+    result = c.div(4, 2)
+
+    assert result == 2
+
+def test_div_two_num_float():
+    c = pt.Calc()
+    result = c.div(15, 2)
+    assert result == 7.5
+
+def test_zero():
+    c = pt.Calc()
+    result = c.zero(4, 0)
+
+    assert result == "inf"
+
+def test_mu_by_zero_raises_exception():
+    with pytest.raises(ValueError):
+        pt.Calc().mul(3, 0)
+
+    #assert Calc().div
+    #assert Calc().div(13, 2)  6.5
+
+def test_list_empty():
+    result = "is empty"
+    assert pt.Calc().is_empty
+
+def test_average():
+    assert pt.Calc().avg([2, 5,12, 98]) == 20.25
+
+def remove_upper():
+    assert pt.Calc().avg([2, 5,12, 98])
+
+"""
+average of an iterable
+account for empty
+upper and lower threshhold work seperatly
+
+"""
